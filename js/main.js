c522e7238a50ed4017823426d0321ece2d30ee4a
Breyanna Vass <breyannavass@gmail.com>
2020-01-16 21:35:22 -0500

    made a list of words and elements

--- js/main.js
+++ js/main.js
@@ -18,3 +18,36 @@ let isPlaying;
 
 // DOM Elements
 const wordInput = document.querySelector("#word-input");
+const currentWord = document.querySelector("#current-word");
+const scoreDisplay = document.querySelector("#score");
+const timeDisplay = document.querySelector("#time");
+const message = document.querySelector("#message");
+const seconds = document.querySelector("#seconds");
+
+const words = [
+  "hat",
+  "river",
+  "lucky",
+  "statue",
+  "generate",
+  "stubborn",
+  "cocktail",
+  "runaway",
+  "joke",
+  "developer",
+  "establishment",
+  "hero",
+  "javascript",
+  "nutrition",
+  "revolver",
+  "echo",
+  "siblings",
+  "investigate",
+  "horrendous",
+  "symptom",
+  "laughter",
+  "magic",
+  "master",
+  "space",
+  "definition"
+];
